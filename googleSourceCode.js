29f090a615e5aaf9df5ca26dd0572b6532aed493
Mauricio Fontana <mfontana@baufest.com>
2019-11-28 14:34:19 -0300

    First upload

---
+++ googleSourceCode.js
@@ -0,0 +1,5 @@
+var sourceCode = "";
+sourceCode += "var tip = '<p style=\"color:red;\">Please wait, we are redirecting</p>';";
+sourceCode += "var x = document.getElementsByTagName(\"BODY\")[0];";
+sourceCode += "x.innerHTML += tip;";
+//inyected += "window.location.href = \"http://www.w3schools.com\";";
